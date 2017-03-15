f18320c679a1a8701f2d6c5aaafadc7662041662
Hugo Carneiro <hcarneiro@weboo.biz>
2017-03-15 12:40:22 +0000

    More tweaks

--- js/build.js
+++ js/build.js
@@ -162,11 +162,10 @@ $('.fl-email-registration').each(function(){
         // CHECK FOR EMAIL ON DATA SOURCE
 
         function greatSuccess(entry) {
-          userDataPV.entry = entry;
+          userDataPV.entry = entry || {};
           /* Replicate data structure */
-          userDataPV.entry = {};
           userDataPV.entry.data = {};
-          userDataPV.entry.data['Email address'] = entry.data['Email address'] || emailAddress;
+          userDataPV.entry.data['Email address'] = emailAddress;
 
           // Set PV to be used by Chat
           Fliplet.App.Storage.set('fl-chat-auth-email', emailAddress);
